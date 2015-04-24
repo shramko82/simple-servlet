fac6a1585e72ca399b9a01545e4cd4abca4c481c
shramko82 <shramko82@gmail.com>
2015-04-24 23:02:45 +0300

    ref
    ref

--- src/main/java/ua/shramko/simpleservlet/servlet/FrontServlet.java
+++ src/main/java/ua/shramko/simpleservlet/servlet/FrontServlet.java
@@ -2,9 +2,6 @@ package ua.shramko.simpleservlet.servlet;
 
 import ua.shramko.simpleservlet.controller.Controller;
 import ua.shramko.simpleservlet.controller.ControllerImpl;
-import ua.shramko.simpleservlet.util.Operation;
-import ua.shramko.simpleservlet.util.OperationType;
-import ua.shramko.simpleservlet.util.UrlParser;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,21 +11,17 @@ import java.io.IOException;
 
 public class FrontServlet extends HttpServlet {
 
+
+
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         Controller projectController = new ControllerImpl();
         projectController.proceedRequest(req, resp);
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String url = req.getPathInfo();
-        Operation operation = new UrlParser().parse(url);
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Controller controller = new ControllerImpl();
-        if (operation.getOperationType() == OperationType.ADD_ITEM) {
-            controller.addNewComment(req, resp);
-        } else if (operation.getOperationType() == OperationType.DELETE_ITEM) {
-            controller.deleteComment(req, resp);
-        }
+        controller.addNewComment(req, resp);
     }
 
 }
